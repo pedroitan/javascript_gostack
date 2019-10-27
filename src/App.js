444377fc61df0f88d6015526f2bca5845b63faf4
Pedro <pedro@iMac-de-Pedro.local>
2019-10-27 16:55:21 -0300

    chore: Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Techlist from './components/TechList';
+import profile from './assets/a.png';
+
+function App() {
+  // return <img width="1000" src={profile}/>
+  return <Techlist />
+}
+
+export default App;
